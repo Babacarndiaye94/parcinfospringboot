d966825f57fc8ee2cce5a131582dc44121c844df
Babacarndiaye94 <bacusndiaye@gmail.com>
2022-08-23 00:06:16 +0000

    examen a rendre springboot

---
+++ src/main/java/sn/isi/parcinfo/entities/Role.java
@@ -0,0 +1,35 @@
+package sn.isi.parcinfo.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(length = 100, nullable = false)
+    private String nom;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+}
